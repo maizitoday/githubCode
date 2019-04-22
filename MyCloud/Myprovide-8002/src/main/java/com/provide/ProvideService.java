85d647a1785937ba7547c3bbf78972a2c5b68817
maizitoday <maizi_today@sina.com>
2019-04-22 14:44:42 +0800

    Eureka

---
+++ MyCloud/Myprovide-8002/src/main/java/com/provide/ProvideService.java
@@ -0,0 +1,23 @@
+package com.provide;
+
+import entity.MaiziUser;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yubo
+ * @version V2.0
+ * @Description:
+ * @date 2019/4/18 上午5:31
+ * @Company: cecsm.com
+ * @Copyright Copyright (c) 2017
+ */
+@Service
+public class ProvideService {
+
+	public MaiziUser buildData()
+	{
+		MaiziUser user = new MaiziUser();
+		user.setName("小强").setAge(28).setSex("女");
+		return user;
+	}
+}
